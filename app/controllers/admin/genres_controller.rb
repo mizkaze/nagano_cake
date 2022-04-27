33e2db1d969d7d221333949d1780ce42f86d1b95
mizkaze <watashi03go@gmail.com>
2022-04-27 16:41:04 +0000

    [Save]

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,27 +1,34 @@
 class Admin::GenresController < ApplicationController
 
   def new
-    @genre = Genre.new
+    @new_genre = Genre.new
   end
 
   def create
-    @genre = Genre.new(genre_params)
-    @genre.save
+    @new_genre = Genre.new(genre_params)
+    @new_genre.save
     redirect_to admin_genres_path
   end
 
   def index
-    @genre = Genre.new
+    @new_genre = Genre.new
+    @genres = Genre.all
   end
 
   def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    @genre.update(genre_params)
+    redirect_to admin_genres_path
   end
 
 
   private
 
   def genre_params
-    # エラー
     params.require(:genre).permit(:name)
   end
 
--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,5 +1,13 @@
 class Admin::ItemsController < ApplicationController
+
   def new
+    @new_item = Item.new
+  end
+
+  def create
+    @new_item = Item.new(item_params)
+    @new_item.save
+    redirect_to admin_item_path(@new_item.id)
   end
 
   def index
@@ -10,4 +18,12 @@ class Admin::ItemsController < ApplicationController
 
   def edit
   end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :introduction, :price)
+  end
+
 end
