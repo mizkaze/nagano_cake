e38b438ab9465d755a5ffb8a603eced997c20511
mizkaze <watashi03go@gmail.com>
2022-04-27 07:02:49 +0000

    [Complete]

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
 
-  devise_for :admins
-  devise_for :customers
+  # 設計通りのURLになるように修正
+  devise_for :admin,skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
+  # 設計通りのURLになるように修正
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
+
   devise_for :users
 
   root to: "homes#top"
