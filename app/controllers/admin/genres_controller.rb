d7fe9b4da3a885a1f2a88e51e0382a413fa1267a
mizkaze <watashi03go@gmail.com>
2022-04-27 11:55:45 +0000

    [Save]genre_index.html.erb

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,7 +1,28 @@
 class Admin::GenresController < ApplicationController
+
+  def new
+    @genre = Genre.new
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+    @genre.save
+    redirect_to admin_genres_path
+  end
+
   def index
+    @genre = Genre.new
   end
 
   def edit
   end
+
+
+  private
+
+  def genre_params
+    # エラー
+    params.require(:genre).permit(:name)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   end
 
   namespace :admin do
-    resources :genres, only: [:index, :edit]
+    resources :genres, only: [:create, :index, :edit]
   end
 
 
