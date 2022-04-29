efa196819dcc6ea21b71adc448cc95f5bf48fd5f
mizkaze <watashi03go@gmail.com>
2022-04-29 15:19:12 +0000

    [save]show_html

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -24,7 +24,7 @@ class Admin::ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :introduction, :price, :genre_id, :is_active)
+    params.require(:item).permit(:name, :introduction, :price, :genre_id, :is_active, :image)
   end
 
 end
