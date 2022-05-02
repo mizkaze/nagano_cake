0c3fc5a260a059eed618580e5b167adbbc4438fb
mizlkaze <watashi03go@gmail.com>
2022-05-02 08:21:33 +0000

    [Save]show_edit_html

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -5,6 +5,13 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to public_customer_path(@customer.id)
   end
 
   def delete_confirm
@@ -13,6 +20,10 @@ class Public::CustomersController < ApplicationController
 
   private
 
-
+  def customer_params
+    params.require(:customer).permit(
+      :last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number
+    )
+  end
 
 end
