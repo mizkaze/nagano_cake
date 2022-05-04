c3ccde1a103b696ca7af148f99175ff48c01f2e8
mizlkaze <watashi03go@gmail.com>
2022-05-04 13:30:28 +0000

    [Save]

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,10 @@
 class Public::ItemsController < ApplicationController
   def index
+    @genres = Genre.all
   end
 
   def show
+    @genres = Genre.all
+    @item = Item.find(params[:id])
   end
 end
