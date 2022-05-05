2e67497b57cd35984cf4d4cbfa985b2e50677c96
mizlkaze <watashi03go@gmail.com>
2022-05-05 08:46:07 +0000

    [Fix]genre_search

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,12 @@
 class Public::ItemsController < ApplicationController
   def index
     @genres = Genre.all
-    @item = Item.all
+    if params[:genre_id]
+      @item = Item.where(genre_id: params[:genre_id], is_active: true)
+      # 「genre_id:」はカラム名、「[:genre_id]」は自分で命名した”なんでもいい”名前
+    else
+      @item = Item.where(is_active: true)
+    end
   end
 
   def show
