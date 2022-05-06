0892fbef23367df7de87790dfa389f53da1c677c
mizlkaze <watashi03go@gmail.com>
2022-05-06 10:12:33 +0000

    [Save]item/show_html

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -6,9 +6,6 @@ class Public::CartItemsController < ApplicationController
 
   def create
     @new_cart_item = CartItem.new(cart_item_params)
-
-    # item_id取得の記述？
-
     @new_cart_item.customer_id = current_customer.id
     @new_cart_item.save
     redirect_to public_cart_items_index_path
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,4 +1,16 @@
 class Public::ItemsController < ApplicationController
+
+  def new
+    @new_cart_item = CartItem.new
+  end
+
+  def create
+    @new_cart_item = CartItem.new(cart_item_params)
+    @new_cart_item.customer_id = current_customer.id
+    @new_cart_item.save
+    redirect_to public_cart_items_index_path
+  end
+
   def index
     @genres = Genre.all
     if params[:genre_id]
@@ -13,5 +25,11 @@ class Public::ItemsController < ApplicationController
   def show
     @genres = Genre.all
     @item = Item.find(params[:id])
+  end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount)
   end
 end
