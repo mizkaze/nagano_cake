b57fce3b508c8b20ca64becd4cfaec64a14d92bb
mizlkaze <watashi03go@gmail.com>
2022-05-17 08:41:12 +0000

    preparation_enum

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,7 @@ class Order < ApplicationRecord
 
   belongs_to :customer
 
-  enum payment_method: { credit_card: 0, transfer: 1 }
+  enum method_of_payment: { credit_card: 0, transfer: 1 }
+  enum status: { hold: 0, verified: 1, making: 2, before_shipping: 3, shipped: 4 }
 
 end
