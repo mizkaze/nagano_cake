d1b49fefcdb08619faff73957a21de1306409dca
mizlkaze <watashi03go@gmail.com>
2022-05-20 11:23:37 +0000

    [Save]new_html

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -4,12 +4,40 @@ class Public::OrdersController < ApplicationController
   end
 
   def create
-    new_order = Order.new(order_params)
-    new_order.save
+    # Orderモデルに必要な情報を用意する
+    @new_order = current_customer.new(order_params)
+    @new_order.save
     redirect_to confirm_public_orders_path
   end
 
   def confirm
+    @new_order = Order.new(order_params)
+
+    if params[:order][:address_number] == "1"
+      @new_order.name = current_customer.name
+      @new_order.address = current_customer.address
+
+    elsif params[:order][address_number] == "2"
+      if Address.exists?(name: params[:order][:registered])
+        @new_order.name = Address.find(params[:order][:registered]).name
+        @new_order.address = Address.find(params[:order][:registered]).address
+      else
+        render :new
+      end
+
+    elsif params[:order][:address_number] == "3"
+      new_address = current_customer.address.new(address_params)
+      if new_address.save
+      else
+        render :new
+      end
+
+    else
+      redirect_to root_path
+    end
+
+    @cart_items = current_customer.cart_items.all
+    @total = @cart_items.inject(0) { |sum, item| sum + item.sum_price }
   end
 
   def thanks
@@ -28,5 +56,9 @@ class Public::OrdersController < ApplicationController
     params.require(:order).permit(
       :name, :postal_code, :address, :postage, :total_payment, :method_of_payment, :status
     )
+  end
+
+  def address_params
+    params.require(:order).permit(:name, :poatal_code, :address)
   end
 end
