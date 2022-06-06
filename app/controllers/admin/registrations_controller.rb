be14b3d039923107f87152d6441f2aa4e3e70ac0
mizlkaze <watashi03go@gmail.com>
2022-06-06 06:13:20 +0000

    [Fix]admin_sign_out_for

--- app/controllers/admin/registrations_controller.rb
+++ app/controllers/admin/registrations_controller.rb
@@ -60,11 +60,11 @@ class Admin::RegistrationsController < Devise::RegistrationsController
   #   super(resource)
   # end
 
-  def after_sign_in_path_for(resource)
+  # registrationsには、「新規作成」に関する記述をする
+  # ログイン・ログアウトについては「sessions」
+  def after_sign_up_path_for(resource)
     admin_root_path
   end
 
-  def after_sign_out_path_for(resource)
-    new_admin_session_path
-  end
+
 end
